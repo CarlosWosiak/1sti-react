f29585576d49451cba198466691659b2d3f139bb
Carlos Wosiak <carlosw403@hotmail.com>
2020-05-17 20:14:26 -0300

    initial configuration

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import LateralNavbar from './components/lateral-navbar/lateral-navbar';
+import Routes from './routes';
+
+function App() {
+  return (
+    <div>
+        <LateralNavbar/>
+        <Routes/>
+    </div>
+  );
+}
+
+export default App;
